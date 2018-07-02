3bb69a72c816d14e59785cf9a02b85776a482d5e
Satyajit Das <satyajitd1998@gmail.com>
2018-07-02 11:08:00 +0530

    Add files via upload

---
+++ face_detect_img.py
@@ -0,0 +1,20 @@
+
+import cv2 as cv
+from matplotlib import pyplot as plt
+import time
+import copy
+
+path = input("Enter the path of the image to be processed: ")
+img = cv.imread(path) # reads image in GBR mode
+img_copy = copy.copy(img)
+gray_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_alt.xml')
+face = face_cascade.detectMultiScale(gray_img, 1.2, 8)
+
+for (x,y,w,h) in face :
+    cv.rectangle(img_copy,(x,y),(x+w,y+h), color = (0,0,255), thickness=4)
+plt.axis("off")
+plt.imshow(cv.cvtColor(img_copy, cv.COLOR_BGR2RGB))
+plt.title('Image')
+plt.show()
