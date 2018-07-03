5e8c882e1813119c09dde8891d66b2acd21fde86
Satyajit Das <satyajitd1998@gmail.com>
2018-07-03 10:52:10 +0530

    Update face_detect_vid.py

--- face_detect_vid.py
+++ face_detect_vid.py
@@ -14,7 +14,8 @@ def detect (frame, f_c, scale_factor, minneigh) :
     faces = f_c.detectMultiScale(gray_img,scale_factor,minneigh)
     for (x,y,w,h) in faces :
         cv.rectangle(frame,(x,y),(x+w,y+h), color = (0,0,255), thickness=2)
-        cv.putText(frame, label, (x, y-10),cv.FONT_HERSHEY_SIMPLEX, fontScale = 0.4, color=(0,0,255), thickness=1);
+        cv.putText(frame, label, (x, y-10),cv.FONT_HERSHEY_SIMPLEX, fontScale = 0.4, color=(0,0,255), thickness=1)
+        cv.putText(frame, " Press 'q' to quit", (0+2, 0+15),cv.FONT_HERSHEY_SIMPLEX, fontScale = 0.5, color=(255,0,0), thickness=1)
 
 capture = cv.VideoCapture(0) # 0 -> webcam
 
