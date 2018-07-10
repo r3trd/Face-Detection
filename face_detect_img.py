2732c6effe4a0ac99edf026e89631b5491a6dbbe
Satyajit Das <satyajitd1998@gmail.com>
2018-07-10 20:28:16 +0530

    Update face_detect_img.py

--- face_detect_img.py
+++ face_detect_img.py
@@ -10,7 +10,7 @@ img_copy = copy.copy(img)
 gray_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_alt.xml')
-face = face_cascade.detectMultiScale(gray_img, 1.2, 8)
+face = face_cascade.detectMultiScale(gray_img, 1.2, 5)
 
 for (x,y,w,h) in face :
     cv.rectangle(img_copy,(x,y),(x+w,y+h), color = (0,0,255), thickness=4)
